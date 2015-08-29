149956213ff3577cd309231f3899cd041823477b
Nil Point <nilpoint@hotmail.com>
2015-08-29 16:32:53 +0800

    Change the answer text when shaking the device.

--- EightBall/ViewController.swift
+++ EightBall/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var answerView: UITextView!
+  let answers = [ "\rYES", "\rNO", "\rMAYBE",
+    "I\rDON'T\rKNOW", "TRY\rAGAIN\rSOON", "READ\rTHE\rMANUAL" ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,7 +23,40 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  // Motion
+  override func motionBegan(motion: UIEventSubtype, withEvent event: UIEvent) {
+    if motion == .MotionShake {
+      fadeFortune()
+    }
+  }
+  
+  override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+    if motion == .MotionShake {
+      newFortune()
+    }
+  }
+  
+  override func motionCancelled(motion: UIEventSubtype, withEvent event: UIEvent) {
+    if motion == .MotionShake {
+      newFortune()
+    }
+  }
 
+  // Helper func
+  func fadeFortune() {
+    UIView.animateWithDuration(0.75, animations: { () -> Void in
+      self.answerView.alpha = 0.0
+    });
+  }
+  
+  func newFortune() {
+    let randomIndex = Int(arc4random_uniform(UInt32(self.answers.count)))
+    answerView.text = answers[randomIndex]
+    UIView.animateWithDuration(2.0, animations: { () -> Void in
+        self.answerView.alpha = 1.0;
+      });
+  }
 
 }
 
